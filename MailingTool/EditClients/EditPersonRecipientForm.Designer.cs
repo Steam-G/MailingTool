246d7d4278806e755c8f766c1e5de38462bc1f0f
L_Andrey <l.andrey@npfgorizont.ru>
2019-10-02 17:35:03 +0500

    Переместил форму редактирования данных пользователя и на ее основе создал форму редактирования получателя рассылки и заменил заколовки в таблице.

---
+++ MailingTool/EditClients/EditPersonRecipientForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MailingTool
+{
+    partial class EditPersonRecipientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.recipientNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.organizationTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.eMailTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.noteTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(146, 145);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 15;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(65, 145);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 14;
+            this.okButton.Text = "Сохранить";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // recipientNameTextBox
+            // 
+            this.recipientNameTextBox.Location = new System.Drawing.Point(69, 38);
+            this.recipientNameTextBox.Name = "recipientNameTextBox";
+            this.recipientNameTextBox.Size = new System.Drawing.Size(183, 20);
+            this.recipientNameTextBox.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Имя получателя";
+            // 
+            // organizationTextBox
+            // 
+            this.organizationTextBox.Location = new System.Drawing.Point(69, 12);
+            this.organizationTextBox.Name = "organizationTextBox";
+            this.organizationTextBox.Size = new System.Drawing.Size(183, 20);
+            this.organizationTextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Организация";
+            // 
+            // eMailTextBox
+            // 
+            this.eMailTextBox.Location = new System.Drawing.Point(69, 64);
+            this.eMailTextBox.Name = "eMailTextBox";
+            this.eMailTextBox.Size = new System.Drawing.Size(183, 20);
+            this.eMailTextBox.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "E-mail";
+            // 
+            // noteTextBox
+            // 
+            this.noteTextBox.Location = new System.Drawing.Point(69, 90);
+            this.noteTextBox.Name = "noteTextBox";
+            this.noteTextBox.Size = new System.Drawing.Size(183, 20);
+            this.noteTextBox.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Заметки";
+            // 
+            // EditPersonRecipientForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(265, 180);
+            this.Controls.Add(this.noteTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.eMailTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.recipientNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.organizationTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MinimizeBox = false;
+            this.Name = "EditPersonRecipientForm";
+            this.ShowInTaskbar = false;
+            this.Text = "EditPersonForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.TextBox recipientNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox organizationTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox eMailTextBox;
+        private System.Windows.Forms.Label label4;
+
+        public EditPersonRecipientForm()
+        {
+        }
+
+        private System.Windows.Forms.TextBox noteTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ MailingTool/EditClients/EditPersonRecipientForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailingTool
+{
+    public partial class EditPersonRecipientForm : Form
+    {
+        PersonRecipient personRecipient;
+
+        public EditPersonRecipientForm(PersonRecipient personRecipient)
+        {
+            InitializeComponent();
+
+            this.personRecipient = personRecipient;
+            organizationTextBox.Text = personRecipient.Organization;
+            recipientNameTextBox.Text = personRecipient.RecipientName;
+            eMailTextBox.Text = personRecipient.EMail;
+            noteTextBox.Text = personRecipient.Note;
+        }
+
+        public PersonRecipient EditedPerson
+        {
+            get { return personRecipient; }
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            personRecipient.Organization = organizationTextBox.Text;
+            personRecipient.RecipientName = recipientNameTextBox.Text;
+            personRecipient.EMail = eMailTextBox.Text;
+            personRecipient.Note = noteTextBox.Text;
+        }
+    }
+}
