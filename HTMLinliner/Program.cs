b3b8cdd07e752d851e1f9160dd4cfb86a42e3b99
L_Andrey <l.andrey@npfgorizont.ru>
2019-12-18 18:51:09 +0500

    Собрал проект HTMLinliner, который объединяет HTML и его стили в один документ.

---
+++ HTMLinliner/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLinliner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Catalog();
+            CreateInlineHTML();
+            
+            Console.ReadKey();
+        }
+
+        static string subpathCSS = @"css";
+        static string subpathHTML = @"HTML";
+        static string subpathHTMLinline = @"HTMLinline";
+
+        static void Catalog()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(subpathHTML);
+            dirInfo.CreateSubdirectory(subpathHTML+"/"+subpathCSS);
+            dirInfo.CreateSubdirectory(subpathHTMLinline);
+        }
+
+        static void CreateInlineHTML()
+        {
+            var files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + subpathHTML);
+            
+            Uri baseURI = new Uri(AppDomain.CurrentDomain.BaseDirectory + subpathHTML + "/" + subpathCSS);
+            DirectoryInfo directoryInfo = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + subpathHTML);
+            foreach (var s in directoryInfo.GetFiles())
+            {
+                Console.WriteLine(s);
+                string htmlSource = File.ReadAllText(s.FullName);
+                var result = PreMailer.Net.PreMailer.MoveCssInline(baseURI, htmlSource);
+                File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + subpathHTMLinline + @"\inline_" + s.Name, result.Html);
+                //Console.WriteLine(result.Warnings); 	// string[] of any warnings that occurred during processing.
+            }
+
+            //Console.WriteLine(result.Html);         // Resultant HTML, with CSS in-lined.
+            //Console.WriteLine(result.Warnings); 	// string[] of any warnings that occurred during processing.
+            //Console.ReadKey();
+
+            Console.WriteLine("\n Готово! Все HTML-документы объеденены с их стилями...");
+        }
+    }
+}
--- other/PreMailer.Net-master/PreMailer.Net-master/PreMailer.Net/TestPM/Program.cs
+++ other/PreMailer.Net-master/PreMailer.Net-master/PreMailer.Net/TestPM/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PreMailer.Net;
 
 namespace TestPM
 {
@@ -11,10 +12,16 @@ namespace TestPM
 	{
 		static void Main(string[] args)
 		{
-			string htmlSource = File.ReadAllText(@"C:\Workspace\testmail.html");
-			PreMailer.Net.PreMailer pm;
-			string premailedOutput = pm.MoveCssInline(htmlSource, false);
-			///string premailedOutput = pm.MoveCssInline(false,htmlSource)
+			string htmlSource = File.ReadAllText(@"C:\Work\Repos\MilingTool\MailingTool\EmailTemplate\simple_ru_сутки.html");
+			string cssSource = File.ReadAllText(@"C:\Work\Repos\MilingTool\MailingTool\EmailTemplate\css\simple.css");
+			Uri baseURI = new Uri(@"C:\Work\Repos\MilingTool\MailingTool\EmailTemplate\");
+			var result = PreMailer.Net.PreMailer.MoveCssInline(baseURI, htmlSource);
+
+			Console.WriteLine(result.Html);         // Resultant HTML, with CSS in-lined.
+													//Console.WriteLine(result.Warnings); 	// string[] of any warnings that occurred during processing.
+			Console.ReadKey();
+
+			File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory+"htmlInLine.html", result.Html);
 		}
 	}
 }
