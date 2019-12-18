2c648076e7bf2eba0dc8a8089adcaea4548f64e8
l.andrey@npfgorizont.ru <l.andrey@npfgorizont.ru>
2019-12-18 11:59:57 +0500

    Добавил проект по превращению HTML и CSS в единое строковое представление, надо проверить как работает и можно будет применять.

---
+++ other/PreMailer.Net-master/PreMailer.Net-master/PreMailer.Net/PreMailer.Net/Sources/LinkTagCssSource.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using AngleSharp.Dom;
+using PreMailer.Net.Downloaders;
+
+namespace PreMailer.Net.Sources
+{
+	public class LinkTagCssSource : ICssSource
+	{
+		private readonly Uri _downloadUri;
+		private string _cssContents;
+
+		public LinkTagCssSource(IElement node, Uri baseUri)
+		{
+			// There must be an href
+			var href = node.Attributes.First(a => a.Name.Equals("href", StringComparison.OrdinalIgnoreCase)).Value;
+
+			if (Uri.IsWellFormedUriString(href, UriKind.Relative) && baseUri != null)
+			{
+				_downloadUri = new Uri(baseUri, href);
+			}
+			else
+			{
+				// Assume absolute
+				_downloadUri = new Uri(href);
+			}
+		}
+
+		public string GetCss()
+		{
+			if (IsSupported(_downloadUri.Scheme))
+			{
+                try
+                {
+                    return _cssContents ?? (_cssContents = WebDownloader.SharedDownloader.DownloadString(_downloadUri));
+                } catch (System.Net.WebException)
+                {
+                    throw new System.Net.WebException(string.Format("PreMailer.Net is unable to fetch the requested URL: {0}", _downloadUri));
+                }
+            }
+			return string.Empty;
+		}
+
+		private bool IsSupported(string scheme)
+		{
+			return
+				scheme == "http" ||
+				scheme == "https" ||
+				scheme == "ftp" ||
+				scheme == "file";
+		}
+	}
+}
---
+++ other/PreMailer.Net-master/PreMailer.Net-master/PreMailer.Net/TestPM/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPM
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string htmlSource = File.ReadAllText(@"C:\Workspace\testmail.html");
+			PreMailer.Net.PreMailer pm;
+			string premailedOutput = pm.MoveCssInline(htmlSource, false);
+			///string premailedOutput = pm.MoveCssInline(false,htmlSource)
+		}
+	}
+}
